c8da920872dd9980c2e0abde6b4d6948855cfc27
Marc Fischer <spam@totallyawesome.at>
2016-10-25 11:06:14 +0200

    updated includes so that both, unmanagedDependencies (breeze) and managed Dependecies (TinyIR) can be used

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,7 +2,10 @@
   * Created by marc on 24/10/16.
   */
 
+
 import ch.ethz.dal.tinyir.processing.Document
+import breeze.linalg.{Vector, DenseVector}
+
 object Main{
   def main(args : Array[String]): Unit ={
     println("Hello, world!")
