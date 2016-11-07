cb68a89906eb91a70b1e531c189837d1d05a28af
Philip Junker <junkerp@ethz.ch>
2016-11-07 17:31:39 +0100

    naive bayes update

--- src/main/scala/NaiveBayes.scala
+++ src/main/scala/NaiveBayes.scala
@@ -21,15 +21,15 @@ object NaiveBayes{
     * @Param wordCountVector
     * @return The probability of the code in the training set
     */
-  def getPwc(theta: DenseVector[Double], alpha: Double, code: String, bowStream: Stream[DataPoint], wordCountVector: SparseVector[Double]):
-      SparseVector[Double] = {
-    if (bowStream.isEmpty) theta / (sum(theta) + alpha*sum(wordCountVector))
+  def getPwc(theta: DenseVector[Double], alpha: Double, code: String, bowStream: Stream[DataPoint], vocabSize: Double):
+      DenseVector[Double] = {
+    if (bowStream.isEmpty) log(theta) - log(sum(theta) + alpha*vocabSize)
     else{
       val doc = bowStream.head
       if(doc.y contains code){
-        getPwc(theta + doc.x + (alpha)*DenseVector.ones[Double](theta.length), alpha, code, bowStream.tail,wordCountVector)
+        getPwc(theta + doc.x, alpha, code, bowStream.tail,vocabSize)
       }else{
-        getPwc(theta, alpha, code, bowStream.tail, wordCountVector)
+        getPwc(theta, alpha, code, bowStream.tail, vocabSize)
       }
 
     }
@@ -42,18 +42,18 @@ object NaiveBayes{
     var thetas = codes.map((_, DenseVector.ones[Double](r.reducedDictionarySize))).toMap.par
     //println(codes)
     //println(" --- NAIVE BAYES : P(CCAT) = " + r.getProbabilityOfCode("CCAT"))
-    val theta = DenseVector.zeros[Double](r.reducedDictionarySize)
+    val theta = SparseVector.zeros[Double](r.reducedDictionarySize)
     val wordcounts = r.getWordCountVector()
     //println(wordcounts)
     //val pwc = getPwc(theta, 1.0, "CCAT", r.toBagOfWords("train"),wordcounts)
     //println(" --- NAIVE BAYES : Pwc calculated for CCAT")
     //println(pwc)
     println(" --- NAIVE BAYES : Calculating P(w|c) all codes in the training set...")
-    thetas = thetas.map { case (code, theta) => code -> getPwc(theta = DenseVector.zeros[Double](r.reducedDictionarySize),
+    thetas = thetas.map { case (code, theta) => code -> getPwc(theta = theta,
                                                                 alpha = 1.0,
                                                                 code = code,
                                                                 bowStream = r.toBagOfWords("train"),
-                                                                wordCountVector = wordcounts)
+                                                                vocabSize = r.reducedDictionarySize)
     }
     //println(thetas)
 
