3d30d707f01fece1f07c8b2252a7283fc2029613
Tobi <tobias@verhulst.fr>
2016-11-13 12:19:11 +0100

    Logistic Regression improvements

--- src/main/scala/LogisticRegression.scala
+++ src/main/scala/LogisticRegression.scala
@@ -15,19 +15,21 @@ object LogisticRegression{
     1.0 / (1.0 + Math.exp(-x))
   }
 
+    var labelTypes = List("topic", "industry", "country")
 
     var thetasMap : Map[String, Map[String, DenseVector[Double]]] = Map()
     var cutoffMap : Map[String, Double] = Map()
 
-    var reader = new Reader()
+    var reader = new Reader(minOccurrence = 1, maxOccurrenceRate = 0.2)
 
     def train(setName : String): Unit = {
       thetasMap.clear()
       cutoffMap.clear()
-      for (labelType <- List("topic", "industry", "country")) {
+      for (labelType <- labelTypes) {
+
         val documents = reader.toBagOfWords(setName)
 
-        val codes = Set[String](reader.codes.toList: _*)
+        val codes = Set[String](reader.codes.toList: _*).intersect(possibleCodes.fromString(labelType))
         thetasMap(labelType) = Map() ++ codes.map((_, DenseVector.fill[Double](reader.reducedDictionarySize + 1)(0.0))).toMap
         var learning_rate = 1.0
 
@@ -43,7 +45,7 @@ object LogisticRegression{
           }
         }
 
-        println("LOGREG : Starting learning...")
+        println(s"LOGREG : Starting learning theta values for label $labelType...")
         var docNr = 0
         var shrink = 0
         for (doc <- documents) {
@@ -60,7 +62,7 @@ object LogisticRegression{
 
         //Find optimal cutoff value such that the proportion of codes assigned is the same in the training and validation sets
         var averageCodesPerDoc = totalCodesAssigned / 50000.0
-        println(s"average codes expected : $averageCodesPerDoc")
+        println(s"average codes expected for type $labelType : $averageCodesPerDoc")
         var cut = new cutoffFinder(averageCodesPerDoc / codes.size) //proportion of codes assigned to total codes
         val validationSet = reader.toBagOfWords("validation")
         docNr = 0
@@ -76,27 +78,28 @@ object LogisticRegression{
     }
 
     def validate() : Unit = {
-      var assignedCodes: Map[Int, (Set[String], collection.immutable.Set[String])] = Map()
+      var assignedCodes: Map[Int,Set[String]] = Map()
+      var realCodes : Map[Int, collection.immutable.Set[String]] = Map()
       //assign codes
-      for (labelType <- List("topic", "industry", "country")) {
+      for (labelType <- labelTypes) {
         for (validationDoc <- reader.toBagOfWords("validation")) {
           if (!assignedCodes.contains(validationDoc.itemid)) {
-            assignedCodes(validationDoc.itemid) = (Set(), validationDoc.y)
+            assignedCodes(validationDoc.itemid) = Set()
+            realCodes(validationDoc.itemid) = validationDoc.y
           }
-          assignedCodes(validationDoc.itemid) = (assignedCodes(validationDoc.itemid)._1 ++
+          assignedCodes(validationDoc.itemid) ++= //adds the codes
             (thetasMap(labelType).map { case (code, theta) => (logistic(theta.dot(validationDoc.x)), code)
-            }.filter(_._1 > cutoffMap(labelType)).map(_._2).toSet), assignedCodes(validationDoc.itemid)._2)
+            }.filter(_._1 > cutoffMap(labelType)).map(_._2).toSet)
         }
 
 
-        val averageAssignedCodes = 1.0 * assignedCodes.map(_._2._1.size).sum / assignedCodes.size
-        println(s"average codes assigned : $averageAssignedCodes") //for verification
-
+        val averageAssignedCodes = 1.0 * assignedCodes.map(_._2.size).sum / assignedCodes.size
+        println(s"average codes assigned per doc in total: $averageAssignedCodes") //for verification
 
         println("Computing score")
-        val validationPrecisionRecall = assignedCodes.map { case (itemid, (actual, expected)) =>
-          (actual.intersect(expected).size.toDouble / (actual.size + scala.Double.MinPositiveValue),
-            actual.intersect(expected).size.toDouble / (expected.size + scala.Double.MinPositiveValue))
+        val validationPrecisionRecall = assignedCodes.map { case (itemid, assigned) =>
+          (assigned.intersect(realCodes(itemid)).size.toDouble / (assigned.size + scala.Double.MinPositiveValue),
+            assigned.intersect(realCodes(itemid)).size.toDouble / (realCodes(itemid).size + scala.Double.MinPositiveValue))
         }
         println(s"average precision : ${validationPrecisionRecall.map(_._1).sum / validationPrecisionRecall.size}")
         println(s"average recall   : ${validationPrecisionRecall.map(_._2).sum / validationPrecisionRecall.size}")
